834f3e5a4206e6ad2187a03de08dafadffb00a95
Pratik Patil <80518259+Pratiks-Patil@users.noreply.github.com>
2023-10-26 14:56:28 +0530

    Update Image_Transform.py

--- Image_Transform.py
+++ Image_Transform.py
@@ -2,7 +2,6 @@ import streamlit as st
 import cv2
 import numpy as np
 
-# Function to perform affine transformations
 def apply_affine_transformation(image, transformation_type, transformation_value):
     height, width = image.shape[:2]
     if transformation_type == 'Translation':
@@ -14,26 +13,22 @@ def apply_affine_transformation(image, transformation_type, transformation_value
     elif transformation_type == 'Scaling':
         scaling_matrix = np.float32([transformation_value, 0, 0], [0, transformation_value, 0])
         transformed_image = cv2.warpAffine(image, scaling_matrix, (width, height))
-    elif transformation_type == 'Shearing':
-        transformed_image = transformed_image = cv2.warpAffine(image, shearing_matrix, (width, height))
+    elif transformation_type == 'flipping':
+        transformed_image = cv2.flip(image, 1)
     else:
         transformed_image = image
     return transformed_image
 
-# Streamlit app
 st.title('Image Transformation App')
 
-# Upload an image
 uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
     image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), cv2.IMREAD_COLOR)
     st.image(image, caption='Uploaded Image', use_column_width=True)
 
-    # Select transformation type
     transformation_type = st.selectbox("Select Transformation Type", ["Translation", "Rotation", "Scaling", "Flipping"])
 
-    # Set transformation value
     if transformation_type == 'Translation':
         transformation_value = st.slider("Select Translation (x, y)", -100, 100, (0, 0))
     elif transformation_type == 'Rotation':
