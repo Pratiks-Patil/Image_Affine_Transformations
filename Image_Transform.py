fecafb39e26dc516a1a1da6f81c671b2aef1d932
Pratik Patil <80518259+Pratiks-Patil@users.noreply.github.com>
2023-10-26 14:43:16 +0530

    Update Image_Transform.py

--- Image_Transform.py
+++ Image_Transform.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import cv2
 import numpy as np
-from io import BytesIO
 
 # Function to perform affine transformations
 def apply_affine_transformation(image, transformation_type, transformation_value):
     height, width = image.shape[:2]
     if transformation_type == 'Translation':
-        translation_matrix = np.float32([1, 0, transformation_value[0]], [0, 1, transformation_value[1]])
+        translation_matrix = np.float32([[1, 0, transformation_value[0]], [0, 1, transformation_value[1]])
         transformed_image = cv2.warpAffine(image, translation_matrix, (width, height))
     elif transformation_type == 'Rotation':
         rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), transformation_value, 1)
@@ -16,8 +15,7 @@ def apply_affine_transformation(image, transformation_type, transformation_value
         scaling_matrix = np.float32([[transformation_value, 0, 0], [0, transformation_value, 0]])
         transformed_image = cv2.warpAffine(image, scaling_matrix, (width, height))
     elif transformation_type == 'Shearing':
-        shearing_matrix = np.float32([1, transformation_value, 0], [transformation_value, 1, 0])
-        transformed_image = cv2.warpAffine(image, shearing_matrix, (width, height))
+        transformed_image = None
     else:
         transformed_image = image
     return transformed_image
@@ -29,7 +27,7 @@ st.title('Image Transformation App')
 uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
-    image = cv2.imdecode(np.fromfile(uploaded_image, np.uint8), cv2.IMREAD_COLOR)
+    image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), cv2.IMREAD_COLOR)
     st.image(image, caption='Uploaded Image', use_column_width=True)
 
     # Select transformation type
@@ -43,7 +41,7 @@ if uploaded_image is not None:
     elif transformation_type == 'Scaling':
         transformation_value = st.slider("Select Scaling Factor", 0.1, 3.0, 1.0)
     else:
-        transformation_value = None
+        transformation_value =None
 
     if st.button("Apply Transformation"):
         transformed_image = apply_affine_transformation(image, transformation_type, transformation_value)
