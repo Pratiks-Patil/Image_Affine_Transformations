ec5fb5d9cc028944446156ed7a82942dea54a7c0
Pratik Patil <80518259+Pratiks-Patil@users.noreply.github.com>
2023-10-29 19:11:36 +0530

    Update Image_Transform.py

--- Image_Transform.py
+++ Image_Transform.py
@@ -2,42 +2,36 @@ import streamlit as st
 import cv2
 import numpy as np
 
-def apply_affine_transformation(image, transformation_type, transformation_value):
+st.title("Image Affine Transformations Yogesh")
+
+image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+if image is not None:
+    image = cv2.imdecode(np.frombuffer(image.read(), np.uint8), cv2.IMREAD_COLOR)
     height, width = image.shape[:2]
-    if transformation_type == 'Translation':
-        translation_matrix = np.float32([1, 0, transformation_value[0]], [0, 1, transformation_value[1]])
-        transformed_image = cv2.warpAffine(image, translation_matrix, (width, height))
-    elif transformation_type == 'Rotation':
-        rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), transformation_value, 1)
-        transformed_image = cv2.warpAffine(image, rotation_matrix, (width, height))
-    elif transformation_type == 'Scaling':
-        scaling_matrix = np.float32([transformation_value, 0, 0], [0, transformation_value, 0])
-        transformed_image = cv2.warpAffine(image, scaling_matrix, (width, height))
-    elif transformation_type == 'flipping':
-        transformed_image = cv2.flip(image, 1)
-    else:
-        transformed_image = image
-    return transformed_image
 
-st.title('Image Transformation App')
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
-uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    translation_matrix = np.float32([[1, 0, 50], [0, 1, 30]])
 
-if uploaded_image is not None:
-    image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), cv2.IMREAD_COLOR)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    angle = st.slider("Rotation Angle (degrees)", -180, 180, 0)
+    rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
 
-    transformation_type = st.selectbox("Select Transformation Type", ["Translation", "Rotation", "Scaling", "Flipping"])
+    scale_x = st.slider("Scale X", 0.1, 3.0, 1.0)
+    scale_y = st.slider("Scale Y", 0.1, 3.0, 1.0)
+    scaling_matrix = np.float32([[scale_x, 0, 0], [0, scale_y, 0]])
 
-    if transformation_type == 'Translation':
-        transformation_value = st.slider("Select Translation (x, y)", -100, 100, (0, 0))
-    elif transformation_type == 'Rotation':
-        transformation_value = st.slider("Select Rotation (degrees)", -180, 180, 0)
-    elif transformation_type == 'Scaling':
-        transformation_value = st.slider("Select Scaling Factor", 0.1, 3.0, 1.0)
-    else:
-        transformation_value = None
+    shear_x = st.slider("Shear X", -1.0, 1.0, 0.0)
+    shear_y = st.slider("Shear Y", -1.0, 1.0, 0.0)
+    shear_matrix = np.float32([[1, shear_x, 0], [shear_y, 1, 0]])
 
-    if st.button("Apply Transformation"):
-        transformed_image = apply_affine_transformation(image, transformation_type, transformation_value)
-        st.image(transformed_image, caption='Transformed Image', use_column_width=True)
+    if st.button("Apply Transformations"):
+        translated_image = cv2.warpAffine(image, translation_matrix, (width, height))
+        rotated_image = cv2.warpAffine(image, rotation_matrix, (width, height))
+        scaled_image = cv2.warpAffine(image, scaling_matrix, (width, height))
+        sheared_image = cv2.warpAffine(image, shear_matrix, (width, height))
+
+        st.image(translated_image, caption="Translated Image", use_column_width=True)
+        st.image(rotated_image, caption="Rotated Image", use_column_width=True)
+        st.image(scaled_image, caption="Scaled Image", use_column_width=True)
+        st.image(sheared_image, caption="Sheared Image", use_column_width=True)
